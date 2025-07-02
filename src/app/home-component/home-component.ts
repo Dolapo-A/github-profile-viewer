8b2e70ad16fef334f7eb12a3639463864631467f
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-07-02 20:16:37 +0100

    Add FontAwesome icons and enhance home component layout

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { InterfaceName } from '../interface-name';
 import { TestService } from '../test-service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-home-component',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
 export class HomeComponent {
+  faTwitter = faTwitter;
+  faSearch = faSearch;
+  faLink = faLink;
+  faBuilding = faBuilding;
+  faLocationDot = faLocationDot;
+
+  username = 'Dolapo-A';
+  siteLists!: InterfaceName[];
+
+  @ViewChild('searchInput') searchInput!: ElementRef;
   constructor(private router: Router, private testService: TestService) {}
   goToAbout() {
     this.router.navigate(['/about']);
   }
 
-  username = 'Dolapo-A'
-  siteLists!: InterfaceName[];
 
   ngOnInit() {
     this.testService.getlist(this.username).subscribe({
@@ -34,5 +49,9 @@ export class HomeComponent {
         console.log('Data fetching completed successfully.');
       },
     });
+  }
+
+  foucusInput() {
+    this.searchInput.nativeElement.focus();
   }
 }
